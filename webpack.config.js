f8335386f2281a89535c3db76f813aef70d50ab5
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-23 02:06:13 -0700

    Revert "WIP build refactor"
    This reverts commit bfdfb6aaf2c74ba483e3ab905ec1fbf654e1e3f5.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./client/bootstrap.js",
+  entry: "./bootstrap.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "client/bootstrap.js",
+    filename: "bootstrap.js",
   },
   mode: "development"
 };
