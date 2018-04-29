a3090223bc8c19e5370794e4334d295a45a76745
Lynn Cyrin <firemagelynn@gmail.com>
2018-04-29 10:59:39 -0700

    add a proper function to the main script :p

--- client/main.ts
+++ client/main.ts
@@ -1,8 +1,13 @@
 import * as Rust from "./assets/built-wasm/galaxy_gen";
 
 export class MainScript {
+  private rust: typeof Rust;
+
   constructor(rust: typeof Rust) {
-    const universe: Uint8Array = rust.show_universe(10, 10);
-    console.log(universe);
+    this.rust = rust;
+  }
+
+  public show_universe(height: number, width: number): Uint8Array {
+    return this.rust.show_universe(10, 10);
   }
 }
