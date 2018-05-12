80a2f076cee426b66ce1b37a86ce3863138a8866
Lynn Cyrin <firemagelynn@gmail.com>
2018-05-11 21:20:24 -0700

    finished basic karma setup

---
+++ client/tests/initalize.spec.ts
@@ -0,0 +1,7 @@
+import "jasmine-expect-count";
+
+describe("Wasm initalization", () => {
+  it("runs karma", () => {
+    expect(true).toBeTruthy();
+  });
+});
--- client/tests/karma.conf.js
+++ client/tests/karma.conf.js
@@ -3,21 +3,28 @@
 
 module.exports = function(config) {
   config.set({
-    basePath: "./../",
+    basePath: "./../../",
     browsers: ["Chrome"],
-    frameworks: ["jasmine"],
+    frameworks: ["jasmine", "karma-typescript"],
     plugins: [
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
-      require("karma-spec-reporter")
+      require("karma-spec-reporter"),
+      require("karma-typescript")
     ],
-    files: ["client/tests/*spec.+(js|ts)"],
-    reporters: ["spec"],
+    files: ["client/tests/*spec.ts"],
+    preprocessors: {
+      "./**/*.ts": "karma-typescript"
+    },
+    reporters: ["spec", "karma-typescript"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
     singleRun: true,
-    concurrency: Infinity
+    concurrency: Infinity,
+    karmaTypescriptConfig: {
+      tsconfig: "client/tests/tsconfig.json"
+    }
   });
 };
