5d459618bf8836f48636376d725c21a9cf84039d
Lynn Cyrin <firemagelynn@gmail.com>
2018-05-14 00:31:05 -0700

    actually fix entry path

--- src/js/webpack.config.js
+++ src/js/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/js/bootstrap.js",
+  entry: path.resolve(__dirname, "bootstrap.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js"
