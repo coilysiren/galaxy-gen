035950167a96b0eaa5b43a2f9021277ddff0b939
[[ BOT ]] Lynn Cyrin <lynncyrin@gmail.com>
2018-06-11 00:07:35 -0700

    always rebase bot commits

--- bin/deploy-wasm-pack.sh
+++ bin/deploy-wasm-pack.sh
@@ -4,6 +4,7 @@
 git init
 git config --global user.email "lynncyrin@gmail.com"
 git config --global user.name "[[ BOT ]] Lynn Cyrin"
+git config --global pull.rebase true
 git remote remove origin
 git remote add origin https://${GITHUB_API_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 git pull origin
