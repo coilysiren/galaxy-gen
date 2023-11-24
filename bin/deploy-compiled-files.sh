ede8b296f5f8fe2bbb2d57d67a3878a10f8a41de
Kai Siren <coilysiren@gmail.com>
2023-11-23 22:09:16 -0500

    wasm build working

--- bin/deploy-compiled-files.sh
+++
@@ -1,2 +0,0 @@
-make build-wasm
-make build-js-prod
--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate specs_derive;
+
 extern crate rand;
 extern crate specs;
 extern crate wasm_bindgen;
@@ -7,6 +8,7 @@ extern crate wasm_bindgen;
 pub mod cell;
 pub mod galaxy;
 pub mod galaxyecs;
+mod utils;
 
 use cell::Cell;
 use galaxy::Galaxy;
