08a46d7e5ef70848ef8c7efa72976c9554071c8a
Kai Siren <coilysiren@gmail.com>
2023-11-23 21:04:10 -0500

    checkpoint-kai-1700791450

--- src/rust/lib.rs
+++ src/rust/lib.rs
@@ -1,15 +1,12 @@
 #[macro_use]
-extern crate cfg_if;
+extern crate specs_derive;
 extern crate rand;
 extern crate specs;
-#[macro_use]
-extern crate specs_derive;
 extern crate wasm_bindgen;
 
 pub mod cell;
 pub mod galaxy;
 pub mod galaxyecs;
-mod utils;
 
 use cell::Cell;
 use galaxy::Galaxy;
